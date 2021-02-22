82ee39003dad11e081d22b167691a97576b53993
Benjamin Bauten <BenjaminBauten@web.de>
2021-02-22 22:01:43 +0100

    SwiftUI-Weather: Project created; Backgroundcolour added

---
+++ Swift/SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Benjamin Bauten on 22/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+                edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Benjamin Bauten on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
