a7c5a3eeb0777024524d96d47694847c13b6ccc8
Benjamin Bauten <BenjaminBauten@web.de>
2021-06-28 13:01:09 +0200

    Journal starter: Project Start

---
+++ Swift/Journal starter/Journal/AppDelegate.swift
@@ -0,0 +1,86 @@
+/// Copyright (c) 2018 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import CoreLocation
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  static let geoCoder = CLGeocoder()
+  var window: UIWindow?
+  
+  let center = UNUserNotificationCenter.current()
+  let locationManager = CLLocationManager()
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    let rayWenderlichColor = UIColor(red: 0/255, green: 104/255, blue: 55/255, alpha: 1)
+    UITabBar.appearance().tintColor = rayWenderlichColor
+    center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+    }
+    locationManager.requestAlwaysAuthorization()
+    locationManager.startMonitoringVisits()
+    locationManager.delegate = self
+    return true
+  }
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+    // create CLLocation from the coordinates of CLVisit
+    let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+    // Get location description
+    AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
+      if let place = placemarks?.first {
+        let description = "\(place)"
+        self.newVisitReceived(visit, description: description)
+      }
+    }
+
+  }
+
+  func newVisitReceived(_ visit: CLVisit, description: String) {
+    let location = Location(visit: visit, descriptionString: description)
+
+    // Save location to disk
+    
+    // 1
+    let content = UNMutableNotificationContent()
+    content.title = "New Journal entry 📌"
+    content.body = location.description
+    content.sound = .default
+
+    // 2
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+
+    // 3
+    center.add(request, withCompletionHandler: nil)
+
+  }
+}
+
